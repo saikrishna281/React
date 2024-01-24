d5288b460a1a4848ef722320d298293cc02fdee4
saikrishnavegi <saikrishnavegi529@gmail.com>
2024-01-24 12:44:03 +0530

    ....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Validation from './Compoents/Validation';
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <Validation/>
     </div>
   );
 }
