bc849bbb27401c9d9ad4b99e321e9de3d6e8138a
saikrishnavegi <saikrishnavegi529@gmail.com>
2024-02-01 11:13:23 +0530

    ..

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -14,16 +14,17 @@ function Table() {
         e.preventDefault()
     }
     const changehandler=e=>{
-        setStudents({[...student,e.target.name]:e.target.name})
+        setStudents(({...student,[e.target.name]:e.target.value}))
+        console.log(student)
     }
   return (
     <div>
         <form onSubmit={handlesubmit}>
      <input type="text" placeholder="Enter FirstName" name='firstname' onChange={changehandler}/><br/>
-     <input type="text" placeholder="Enter LastName" name='lastname'/><br/>
-     <input type="email" placeholder="Enter Email" name='email'/><br/>
-     <input type="text" placeholder="Enter Gender" name='gender'/><br/>
-     <input type="number" placeholder="Enter Contact" name='contact'/><br/>
+     <input type="text" placeholder="Enter LastName" name='lastname'  onChange={changehandler}/><br/>
+     <input type="email" placeholder="Enter Email" name='email'  onChange={changehandler}/><br/>
+     <input type="text" placeholder="Enter Gender" name='gender'  onChange={changehandler}/><br/>
+     <input type="number" placeholder="Enter Contact" name='contact'  onChange={changehandler}/><br/>
      <button>Add Student</button>
      </form>
     </div>
